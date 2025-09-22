b6e178da40d98b920cc2cef759468676b1a00f5b
Gaurav Pandey <gauravp9118@gmail.com>
2025-09-23 00:28:54 +0530

    change the way of importing images

--- src/components/home/GallerySection.jsx
+++ src/components/home/GallerySection.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import image1 from '../../assets/01.jpg';
+import image2 from '../../assets/02.jpg';
+import image3 from '../../assets/03.jpg';
+import image4 from '../../assets/04.jpg';
+import image5 from '../../assets/05.jpg';
+import image6 from '../../assets/06.jpg';
+import image7 from '../../assets/07.jpg';
+import image8 from '../../assets/08.jpg';
+import image9 from '../../assets/09.jpg';
+import image10 from '../../assets/10.jpg';
+import image11 from '../../assets/11.jpg';
+import image12 from '../../assets/12.jpg';
+
+
 
 const GallerySection = () => {
   const images = [
-    '../../assets/01.jpg',
-    '../../assets/02.jpg',
-    '../../assets/03.jpg',
-    '../../assets/04.jpg',
-    '../../assets/05.jpg',
-    '../../assets/06.jpg',
-    '../../assets/07.jpg',
-    '../../assets/08.jpg',
-    '../../assets/10.jpg',
-    '../../assets/11.jpg',
-    '../../assets/12.jpg'
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12
   ];
 
- 
+
 
   // Duplicate arrays for seamless infinite scroll
   const duplicatedImages = [...images, ...images];
